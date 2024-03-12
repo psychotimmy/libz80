130d4da427b097dc521cee79b6294b73f0c9be14
psychotimmy <timholyoake@btopenworld.com>
2024-03-12 20:45:45 +0000

    Code tidy ups - no functional changes
    Code tidy ups - no functional changes

--- z80.c
+++ z80.c
@@ -26,7 +26,6 @@
 #include "z80.h"
 #include "string.h"
 
-
 #define BR (ctx->R1.br)
 #define WR (ctx->R1.wr)
 
@@ -36,19 +35,19 @@
 
 #define VALFLAG(F,V) valFlag(ctx, F, V)
 
-
 /* ---------------------------------------------------------
  *  Flag tricks
  * --------------------------------------------------------- 
  *
- * To avoid repeating entries in the spec files, many operations that look similar are treated as special cases
- * of a more general operation.
+ * To avoid repeating entries in the spec files, many operations that look
+ * similar are treated as special cases of a more general operation.
  *
- * For example, ADD and ADC are similar in syntax and operation - the difference is that ADC takes the carry flag
- * into account.
+ * For example, ADD and ADC are similar in syntax and operation - the 
+ * difference is that ADC takes the carry flag into account.
  *
- * So we define a general operation doArithmetic(...) which accepts a boolean parameter specifying wheter to do
- * a Carry-operation or not. Then, when we parse, we can say
+ * So we define a general operation doArithmetic(...) which accepts 
+ * a boolean parameter specifying whether to do a carry operation or not.
+ * Then, when we parse, we can say:
  *
  * (ADD|ADC) ....
  *		doArithmetic(FLAG_FOR_%1)
@@ -88,11 +87,11 @@ static const int F2_SUB = 1;
 #define INCR (ctx->R = (ctx->R & 0x80) | ((ctx->R + 1) & 0x7f))
 #define DECR (ctx->R = (ctx->R & 0x80) | ((ctx->R - 1) & 0x7f))
 
-
 /* ---------------------------------------------------------
  *  The opcode implementations
  * --------------------------------------------------------- 
  */
+
 #include "codegen/opcodes_decl.h"
 
 typedef enum
@@ -108,10 +107,8 @@ typedef void (*Z80OpcodeFunc) (Z80Context* ctx);
 struct Z80OpcodeEntry
 {
 	Z80OpcodeFunc func;
-	
 	int operand_type;
 	char* format;	
-	
 	const struct Z80OpcodeTable* table;
 };
 
@@ -178,19 +175,19 @@ static void ioWrite (Z80Context* ctx, ushort addr, byte val)
  * --------------------------------------------------------- 
  */
  
-/** Sets a flag */
+/* Sets a flag */
 static void setFlag(Z80Context* ctx, Z80Flags flag)
 {
 	BR.F |= flag;
 }
 
-/** Resets a flag */
+/* Resets a flag */
 static void resFlag(Z80Context* ctx, Z80Flags flag)
 {
 	BR.F &= ~flag;
 }
 
-/** Puts a value in a flag */
+/* Puts a value in a flag */
 static void valFlag(Z80Context* ctx, Z80Flags flag, int val)
 {
 	if (val)
@@ -199,7 +196,7 @@ static void valFlag(Z80Context* ctx, Z80Flags flag, int val)
 		RESFLAG(flag);
 }
 
-/** Returns a flag */
+/* Returns a flag */
 static int getFlag(Z80Context* ctx, Z80Flags flag)
 {
 	return (BR.F & flag) != 0;
@@ -248,14 +245,14 @@ static void adjustFlagSZP (Z80Context* ctx, byte val)
 /* Adjust flags after AND, OR, XOR */
 static void adjustLogicFlag (Z80Context* ctx, int flagH)
 {
-    VALFLAG(F_S, (BR.A & 0x80) != 0);
-    VALFLAG(F_Z, (BR.A == 0));
-    VALFLAG(F_H, flagH);
-    VALFLAG(F_N, 0);
-    VALFLAG(F_C, 0);
-    VALFLAG(F_PV, parityBit[BR.A]);
+	VALFLAG(F_S, (BR.A & 0x80) != 0);
+	VALFLAG(F_Z, (BR.A == 0));
+	VALFLAG(F_H, flagH);
+	VALFLAG(F_N, 0);
+	VALFLAG(F_C, 0);
+	VALFLAG(F_PV, parityBit[BR.A]);
 
-    adjustFlags(ctx, BR.A);
+	adjustFlags(ctx, BR.A);
 }
 
 
@@ -316,14 +313,10 @@ static int condition(Z80Context* ctx, Z80Condition cond)
  
 static int doComplement(byte v)
 {
-	if ((v & 0x80) == 0)
+	if ((v & 0x80) == 0) 
 		return v;
-
-	v = ~v;
-	v &= 0x7F;
-	v++;
-
-	return -v;
+    	else 
+		return -(((~v) & 0x7F)+1);
 }
 
  
@@ -335,13 +328,11 @@ static byte doArithmetic (Z80Context* ctx, byte value, int withCarry, int isSub)
 
 	if (withCarry && GETFLAG(F_C)) ++carryIn;
 
-	if (isSub)
-	{
+	if (isSub) {
 		SETFLAG(F_N);
 		res = BR.A - value - carryIn;
 	}
-	else
-	{
+	else {
 		RESFLAG(F_N);
 		res = BR.A + value + carryIn;
 	}
@@ -367,37 +358,37 @@ static byte doArithmetic (Z80Context* ctx, byte value, int withCarry, int isSub)
 /* Do a 16-bit addition, setting the appropriate flags. */
 static ushort doAddWord(Z80Context* ctx, ushort a1, ushort a2, int withCarry, int isSub)
 {
-	if(withCarry && GETFLAG(F_C))
-		a2++;
-	int sum = a1;
-	if(isSub)
-	{
+        unsigned sum = a1; /* To detect carry */
+
+        if (withCarry && GETFLAG(F_C)) ++a2;
+
+        if (isSub)
 		sum -= a2;
-		VALFLAG(F_H, ((a1 & 0x0fff) - (a2 & 0x0fff)) & 0x1000);
-	}
-	else
-	{
+        else 
 		sum += a2;
-		VALFLAG(F_H, ((a1 & 0x0fff) + (a2 & 0x0fff)) & 0x1000);
-	}
-	VALFLAG(F_C, sum & 0x10000);
-	if(withCarry || isSub)
-	{
-		int minuend_sign = a1 & 0x8000;
-		int subtrahend_sign = a2 & 0x8000;
-		int result_sign = sum & 0x8000;
-		int overflow;
-		if(isSub)
-			overflow = minuend_sign != subtrahend_sign && result_sign != minuend_sign;
-		else
-			overflow = minuend_sign == subtrahend_sign && result_sign != minuend_sign;
-		VALFLAG(F_PV, overflow);
-		VALFLAG(F_S, (sum & 0x8000) != 0);
-		VALFLAG(F_Z, (sum & 0xFFFF) == 0);
-	}
-	VALFLAG(F_N, isSub);
-	adjustFlags(ctx, sum >> 8);
-	return sum;
+
+        VALFLAG(F_H, ((a1 & 0x1000) ^ (a2 & 0x1000) ^ (sum & 0x1000)));
+        VALFLAG(F_C, sum & 0x10000);
+
+        sum &= 0xFFFF;  /* Discard any carry now C and H flags set/reset */
+
+        /* Subtraction of 16bit values is always with carry */
+        if(withCarry) {
+                ushort minuend_sign = a1 & 0x8000;
+                ushort subtrahend_sign = a2 & 0x8000;
+                ushort result_sign = sum & 0x8000;
+                int overflow;
+                if (isSub)
+                        overflow = minuend_sign != subtrahend_sign && result_sign != minuend_sign;
+                else
+                        overflow = minuend_sign == subtrahend_sign && result_sign != minuend_sign;
+                VALFLAG(F_PV, overflow);
+                VALFLAG(F_S, ((sum & 0x8000) != 0));
+                VALFLAG(F_Z, (sum == 0));
+        }
+        VALFLAG(F_N, isSub);
+        adjustFlags(ctx, (byte) (sum >> 8));
+        return (ushort) sum;
 }
 
 
@@ -425,16 +416,16 @@ static void doXOR (Z80Context* ctx, byte value)
 static void doBIT (Z80Context* ctx, int b, byte val)
 {
 	if (val & (1 << b))
-        RESFLAG(F_Z | F_PV);
-    else
-        SETFLAG(F_Z | F_PV);
+        	RESFLAG(F_Z | F_PV);
+	else
+        	SETFLAG(F_Z | F_PV);
 
-    SETFLAG(F_H);
-    RESFLAG(F_N);
+	SETFLAG(F_H);
+	RESFLAG(F_N);
 
-    RESFLAG(F_S);
-    if ((b == 7) && !GETFLAG(F_Z))
-        SETFLAG(F_S);
+	RESFLAG(F_S);
+	if ((b == 7) && !GETFLAG(F_Z))
+        	SETFLAG(F_S);
 }
 
 
@@ -457,144 +448,143 @@ static void doBIT_indexed(Z80Context* ctx, int b, ushort address)
 
 byte doSetRes (Z80Context* ctx, int bit, int pos, byte val)
 {
-    if (bit)
+	if (bit)
 		val |= (1 << pos);
-    else
+	else
 		val &= ~(1 << pos);
-    return val;
+
+	return val;
 }
 
 
 
 static byte doIncDec (Z80Context* ctx, byte val, int isDec)
 {
-    if (isDec)
-    {
-        VALFLAG(F_PV, (val & 0x80) && !((val - 1) & 0x80));
-        val--;
-        VALFLAG(F_H, (val & 0x0F) == 0x0F);
-    }
-    else
-    {
-        VALFLAG(F_PV, !(val & 0x80) && ((val + 1) & 0x80));
-        val++;
-        VALFLAG(F_H, !(val & 0x0F));
-    }
+	if (isDec) {
+		VALFLAG(F_PV, (val & 0x80) && !((val - 1) & 0x80));
+		val--;
+		VALFLAG(F_H, (val & 0x0F) == 0x0F);
+	}
 
-    VALFLAG(F_S, ((val & 0x80) != 0));
-    VALFLAG(F_Z, (val == 0));
-    VALFLAG(F_N, isDec);
+	else {
+		VALFLAG(F_PV, !(val & 0x80) && ((val + 1) & 0x80));
+		val++;
+		VALFLAG(F_H, !(val & 0x0F));
+	}
 
-    adjustFlags(ctx, val);
+	VALFLAG(F_S, (val & 0x80) != 0);
+	VALFLAG(F_Z, (val == 0));
+	VALFLAG(F_N, isDec);
 
-    return val;
+	adjustFlags(ctx, val);
+
+	return val;
 }
 
 
 static byte doRLC (Z80Context* ctx, int adjFlags, byte val)
 {
-    VALFLAG(F_C, (val & 0x80) != 0);
-    val <<= 1;
-    val |= (byte)GETFLAG(F_C);
+	VALFLAG(F_C, (val & 0x80) != 0);
+	val <<= 1;
+	val |= (byte)GETFLAG(F_C);
 
-    adjustFlags(ctx, val);
-    RESFLAG(F_H | F_N);
+	adjustFlags(ctx, val);
+	RESFLAG(F_H | F_N);
 
-    if (adjFlags)
-        adjustFlagSZP(ctx, val);
+	if (adjFlags)
+        	adjustFlagSZP(ctx, val);
 
-    return val;
+	return val;
 }
 
 
 static byte doRL (Z80Context* ctx, int adjFlags, byte val)
 {
-    int CY = GETFLAG(F_C);
-    VALFLAG(F_C, (val & 0x80) != 0);
-    val <<= 1;
-    val |= (byte)CY;
+	int CY = GETFLAG(F_C);
+	VALFLAG(F_C, (val & 0x80) != 0);
+	val <<= 1;
+	val |= (byte)CY;
 
-    adjustFlags(ctx, val);
-    RESFLAG(F_H | F_N);
+	adjustFlags(ctx, val);
+	RESFLAG(F_H | F_N);
 
-    if (adjFlags)
-        adjustFlagSZP(ctx, val);
+	if (adjFlags)
+		adjustFlagSZP(ctx, val);
 
-    return val;
+	return val;
 }
 
 
 static byte doRRC (Z80Context* ctx, int adjFlags, byte val)
 {
-    VALFLAG(F_C, (val & 0x01) != 0);
-    val >>= 1;
-    val |= ((byte)GETFLAG(F_C) << 7);
+	VALFLAG(F_C, (val & 0x01) != 0);
+	val >>= 1;
+	val |= ((byte)GETFLAG(F_C) << 7);
 
-    adjustFlags(ctx, val);
-    RESFLAG(F_H | F_N);
+	adjustFlags(ctx, val);
+	RESFLAG(F_H | F_N);
 
-    if (adjFlags)
-        adjustFlagSZP(ctx, val);
+	if (adjFlags)
+		adjustFlagSZP(ctx, val);
 
-    return val;
+	return val;
 }
 
 
 static byte doRR (Z80Context* ctx, int adjFlags, byte val)
 {
-    int CY = GETFLAG(F_C);
-    VALFLAG(F_C, (val & 0x01));
-    val >>= 1;
-    val |= (CY << 7);
+	int CY = GETFLAG(F_C);
+	VALFLAG(F_C, val & 0x01);
+	val >>= 1;
+	val |= (CY << 7);
 
-    adjustFlags(ctx, val);
-    RESFLAG(F_H | F_N);
+	adjustFlags(ctx, val);
+	RESFLAG(F_H | F_N);
 
-    if (adjFlags)
-        adjustFlagSZP(ctx, val);
+	if (adjFlags)
+		adjustFlagSZP(ctx, val);
 
-    return val;
+	return val;
 }
 
 
 static byte doSL (Z80Context* ctx, byte val, int isArith)
 {
-    VALFLAG(F_C, (val & 0x80) != 0);
-    val <<= 1;
+	VALFLAG(F_C, (val & 0x80) != 0);
+	val <<= 1;
 
-    if (!isArith)
-        val |= 1;
+	if (!isArith)
+		val |= 1;
 
-    adjustFlags(ctx, val);
-    RESFLAG(F_H | F_N);
-    adjustFlagSZP(ctx, val);
+	adjustFlags(ctx, val);
+	RESFLAG(F_H | F_N);
+	adjustFlagSZP(ctx, val);
 
-    return val;
+	return val;
 }
 
 
 static byte doSR (Z80Context* ctx, byte val, int isArith)
 {
-    int b = val & 0x80;
+	int b = val & 0x80;
 
-    VALFLAG(F_C, (val & 0x01) != 0);
-    val >>= 1;
+	VALFLAG(F_C, (val & 0x01) != 0);
+	val >>= 1;
 
-    if (isArith)
-        val |= b;
+	if (isArith)
+		val |= b;
 
-    adjustFlags(ctx, val);
-    RESFLAG(F_H | F_N);
-    adjustFlagSZP(ctx, val);
+	adjustFlags(ctx, val);
+	RESFLAG(F_H | F_N);
+	adjustFlagSZP(ctx, val);
 
-    return val;
+	return val;
 }
 
 
 static void doPush (Z80Context* ctx, ushort val)
 {
-	WR.SP--;
-	WR.SP--;
+	WR.SP -= 2;
 	write16(ctx, WR.SP, val);
 }
 
@@ -603,9 +593,8 @@ static ushort doPop (Z80Context* ctx)
 {
 	ushort val;
 	val = read16(ctx, WR.SP);
-	WR.SP++;
-	WR.SP++;
-    return val;
+	WR.SP += 2;
+	return val;
 }
 
 
@@ -627,20 +616,22 @@ static void doDAA(Z80Context * ctx)
 
         if (GETFLAG(F_N)) {
                 int hd = (cflag || (acu > 0x99));
-                if (GETFLAG(F_H) || (temp > 9)) {
-                        if (temp > 5) RESFLAG(F_H);
+                if (GETFLAG(F_H) || (temp > 0x09)) {
+                        if (temp > 0x05) 
+				RESFLAG(F_H);
                         acu -= 0x06;
                         acu &= 0xff;
                 }
-                if (hd) acu -= 0x160;
+                if (hd) 
+			acu -= 0x160;
         }
-
         else {
-                if (GETFLAG(F_H) || (temp > 9)) {
-                        VALFLAG(F_H, (temp > 9));
+                if (GETFLAG(F_H) || (temp > 0x09)) {
+                        VALFLAG(F_H, (temp > 0x09));
                         acu += 0x06;
                 }
-                if (cflag || ((acu & 0x1f0) > 0x90)) acu += 0x60;
+                if (cflag || ((acu & 0x1f0) > 0x90)) 
+			acu += 0x60;
         }
 
         cflag |=  (acu >> 8) & 0x01;
@@ -673,15 +664,12 @@ static void do_execute(Z80Context* ctx)
 	int offset = 0;
 	ctx->M1PC = ctx->PC;
 
-	do
-	{
-		if (ctx->exec_int_vector)
-		{
+	do {
+		if (ctx->exec_int_vector) {
 			opcode = ctx->int_vector;
 			ctx->tstates += 6;
 		}
-		else
-		{
+		else {
 			ctx->M1 = 1;
 			opcode = read8(ctx, ctx->PC + offset);
 			ctx->M1 = 0;
@@ -691,8 +679,7 @@ static void do_execute(Z80Context* ctx)
 
 		INCR;
 		func = entries[opcode].func;
-		if (func != NULL)
-		{			
+		if (func != NULL) {
 			ctx->PC -= offset;
 			if (ctx->trace)
 				ctx->trace(ctx->memParam);
@@ -700,8 +687,7 @@ static void do_execute(Z80Context* ctx)
 			ctx->PC += offset;
 			break;
 		}
-		else if (entries[opcode].table != NULL)
-		{
+		else if (entries[opcode].table != NULL) {
 			current = entries[opcode].table;
 			entries = current->entries;
 			offset = current->opcode_offset;
@@ -709,8 +695,7 @@ static void do_execute(Z80Context* ctx)
 				DECR;
 		}
 
-		else
-		{
+		else {
 			/* NOP */
 			break;	
 		}
@@ -720,11 +705,10 @@ static void do_execute(Z80Context* ctx)
 
 static void unhalt(Z80Context* ctx)
 {
-    if (ctx->halted)
-    {
-        ctx->halted = 0;
-        ctx->PC++;
-    }
+	if (ctx->halted) {
+		ctx->halted = 0;
+		ctx->PC++;
+	}
 }
 
 
@@ -733,7 +717,7 @@ static void do_nmi(Z80Context* ctx)
 	unhalt(ctx);
 	ctx->IFF2 = ctx->IFF1;
 	ctx->IFF1 = 0;
-    doPush(ctx, ctx->PC);
+	doPush(ctx, ctx->PC);
 	ctx->PC = 0x0066;	
 	ctx->nmi_req = 0;
 	ctx->tstates += 5;
@@ -742,29 +726,26 @@ static void do_nmi(Z80Context* ctx)
 
 static void do_int(Z80Context* ctx)
 {
-    unhalt(ctx);
+	unhalt(ctx);
 	ctx->IFF1 = 0;
 	ctx->IFF2 = 0;
 	ctx->int_req = 0;
-    if (ctx->IM == 0)
-    {
+	if (ctx->IM == 0) {
 		ctx->exec_int_vector = 1;
 		do_execute(ctx);
 		ctx->exec_int_vector = 0;
-    }
-    else if (ctx->IM == 1)
-    {
-        doPush(ctx, ctx->PC);
-        ctx->PC = 0x0038;
+	}
+    	else if (ctx->IM == 1) {
+        	doPush(ctx, ctx->PC);
+        	ctx->PC = 0x0038;
 		ctx->tstates += 7;
-    }
-    else if (ctx->IM == 2)
-    {
-        doPush(ctx, ctx->PC);
+	}
+	else if (ctx->IM == 2) {
+        	doPush(ctx, ctx->PC);
 		ushort vector_address = (ctx->I << 8) | ctx->int_vector;
 		ctx->PC = read16(ctx, vector_address);
 		ctx->tstates += 7;
-    }
+	}
 }
 
 
@@ -774,8 +755,7 @@ void Z80Execute (Z80Context* ctx)
 		do_nmi(ctx);
 	else if (ctx->int_req && !ctx->defer_int && ctx->IFF1)
 		do_int(ctx);
-	else
-	{
+	else {
 		ctx->defer_int = 0;
 		do_execute(ctx);
 	}
@@ -809,16 +789,12 @@ void Z80Debug (Z80Context* ctx, char* dump, char* decode)
 	if (decode)
 		decode[0] = 0;
 
-	do
-	{
+	do {
 		opcode = read8(ctx, PC + offset);
-
 		size++;
-		
 		PC++;
 		fmt = entries[opcode].format;
-		if (fmt != NULL)
-		{			
+		if (fmt != NULL) {
 			PC -= offset;
 			parm = read16(ctx, PC);
 		
@@ -826,8 +802,7 @@ void Z80Debug (Z80Context* ctx, char* dump, char* decode)
 				size++;
 			else
 				size += 2;
-			if (entries[opcode].operand_type != OP_WORD)
-			{
+			if (entries[opcode].operand_type != OP_WORD) {
 				parm &= 0xFF;
 				size--;
 			}
@@ -838,25 +813,21 @@ void Z80Debug (Z80Context* ctx, char* dump, char* decode)
 			PC += offset;
 			break;
 		}
-		else if (entries[opcode].table != NULL)
-		{
+		else if (entries[opcode].table != NULL) {
 			current = entries[opcode].table;
 			entries = current->entries;
 			offset = current->opcode_offset;
 		}
 
-		else
-		{
+		else {
 			if (decode != NULL)
 				strcpy(decode, "NOP (ignored)");
 			break;	
 		}
 	} while(1);	
 	
-	if (dump)
-	{
-		for (offset = 0; offset < size; offset++)
-		{
+	if (dump) {
+		for (offset = 0; offset < size; offset++) {
 			sprintf(tmp, "%02X", read8(ctx, ctx->PC + offset));
 			strcat(dump, tmp);
 		}		
@@ -869,7 +840,8 @@ void Z80RESET (Z80Context* ctx)
 	ctx->PC = 0x0000;
 	BR.F = 0;
 	ctx->IM = 0;
-	ctx->IFF1 = ctx->IFF2 = 0;
+	ctx->IFF1 = 0;
+	ctx->IFF2 = 0;
 	ctx->R = 0;
 	ctx->I = 0;
 	ctx->halted = 0;
